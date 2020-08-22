d5ca2955907d6a9768436b50ac8554046079d21c
ZephyrBlu <lukejholroyd@gmail.com>
2020-08-22 20:35:05 +1200

    Add styling and matchup/player selection

--- src/WinrateSummary.jsx
+++ src/WinrateSummary.jsx
@@ -1,7 +1,9 @@
 import { h } from 'preact';
+import { useState } from 'preact/hooks';
 import './WinrateSummary.css';
 
 const WinrateSummary = ({ name, data }) => {
+    const [winrateDisplayType, setWinrateDisplayType] = useState('matchup');
     const valueComparison = (a, b) => (b - a);
     data.sort(valueComparison);
 
@@ -31,40 +33,55 @@ const WinrateSummary = ({ name, data }) => {
                 <h1 class="WinrateSummary__title">
                     {name}
                 </h1>
-                <div class="WinrateSummary__controls">
-                    <button class="WinrateSummary__control-option">
+                <div
+                    class={`
+                        WinrateSummary__display-controls
+                        ${winrateDisplayType === 'matchup'
+                            ? 'WinrateSummary__display-controls--matchup'
+                            : 'WinrateSummary__display-controls--player'}
+                    `}
+                >
+                    <button
+                        class="WinrateSummary__display-control-option"
+                        onClick={() => setWinrateDisplayType('matchup')}
+                    >
                         By Matchup
                     </button>
-                    <button class="WinrateSummary__control-option">
+                    <button
+                        class="WinrateSummary__display-control-option"
+                        onClick={() => setWinrateDisplayType('player')}
+                    >
                         By Player
                     </button>
                 </div>
             </div>
-            {data.map((d) => (
-                <div class="WinrateSummary__winrate-data">
-                    <h2 class="WinrateSummary__matchup">
-                        {d.matchup}
-                    </h2>
-                    <svg
-                        class={`WinrateSummary__value-bar WinrateSummary__value-bar--${matchupToRace(d.matchup)}`}
-                        viewBox="0 0 101 2"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <line
-                            x1="1"
-                            y1="1"
-                            x2={d.value}
-                            y2="1"
-                            stroke={raceColours[matchupToRace(d.matchup)]}
-                            strokeWidth={0.5}
-                            stroke-linecap="round"
-                        />
-                    </svg>
-                    <h2 class="WinrateSummary__value">
-                        {d.value}%
-                    </h2>
-                </div>
-            ))}
+            <div class="WinrateSummary__winrate-data">
+                {data.map((d) => (
+                    <div class="WinrateSummary__data-point">
+                        <h2 class="WinrateSummary__matchup">
+                            {d.matchup}
+                        </h2>
+                        <svg
+                            class={`WinrateSummary__value-bar WinrateSummary__value-bar--${matchupToRace(d.matchup)}`}
+                            viewBox="0 0 101 2"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <line
+                                x1="1"
+                                y1="1"
+                                x2={d.value}
+                                y2="1"
+                                stroke={raceColours[matchupToRace(d.matchup)]}
+                                strokeWidth={0.5}
+                                stroke-linecap="round"
+                            />
+                        </svg>
+                        <h2 class="WinrateSummary__value">
+                            {d.value}%
+                        </h2>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
